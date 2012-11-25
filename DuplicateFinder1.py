84df05b9b352ae1bf3ba06c02ee7144a35420ca8
Subimal Deb <subimal.deb@gmail.com>
2012-11-25 20:43:24 +0530

    Put in all the other functions as subfunctions of GetDuplicates.

--- DuplicateFinder1.py
+++ DuplicateFinder1.py
@@ -1,74 +1,79 @@
-import hashlib, os, sys
+def GetDuplicates(roots):    
+    '''GetDuplicates prints the list of duplicates to stdout.
+    '''    
+    import hashlib, os 
+    class FileObject():
+        def __init__(self, filename):
+            from os.path import getsize
+    
+            self.name = filename
+            self.size = getsize(self.name)
+            self.md5hex = None
+        def hash(self):
+            if self.md5hex is None:
+                self.md5hex = GetMD5Sum(self.name)
+
+    def FileList(root):
+        '''Generates the list of files in the list "root" 
+           (of directories) by walking through it and its 
+           subfolders.
+        '''
+    
+        for root in roots:
+            m = os.walk(root)
+            for dirpath, subdirs, filenames in m:
+                for name in filenames:
+                    fpath = os.path.join(dirpath, name)
+                    try:
+                        fsize = os.path.getsize(fpath)
+                        yield fpath
+                    except OSError:
+                        pass
+    
+    
+    def GetMD5Sum(filename, chunksize = 25600):
+        '''Returns the md5 hash for the file "file" using 
+           a default buffer size of 25600 bytes.
+        '''
+        f = open(filename,'rb')
+        md5sum = hashlib.md5()
+        eof = False
+    
+        fname = filename.split('/')[-1]
+        # TO DO : make this OS independent
+    
+        while eof == False:
+            oldsum = md5sum.hexdigest()
+            data = f.read(chunksize)
+            md5sum.update(data)
+            if oldsum == md5sum.hexdigest():
+                eof = True
+        f.close()
+        return md5sum.hexdigest()
+    
 
 
 
-def FileList(root):
-    '''Generates the list of files in the list "root" 
-       (of directories) by walking through it and its 
-       subfolders.
-    '''
 
-    for root in roots:
-        m=os.walk(root)
-        for dirpath, subdirs, filenames in m:
-            for name in filenames:
-                fpath=os.path.join(dirpath,name)
-                try:
-                    fsize=os.path.getsize(fpath)
-                    yield fpath
-                except OSError:
-                    pass
-
-def GetMD5Sum(filename, chunksize=25600):
-    '''Returns the md5 hash for the file "file" using 
-       a default buffer size of 25600 bytes.
-    '''
-    f = open(filename,'rb')
-    md5sum = hashlib.md5()
-    eof = False
-
-    fname=filename.split('/')[-1]
-    # TO DO : make this OS independent
-
-    while eof==False:
-        oldsum=md5sum.hexdigest()
-        data = f.read(chunksize)
-        md5sum.update(data)
-        if oldsum==md5sum.hexdigest():
-            eof = True
-    f.close()
-    return md5sum.hexdigest()
-
-class FileObject():
-    def __init__(self, filename):
-        from os.path import getsize
-
-        self.name = filename
-        self.size = getsize(self.name)
-        self.md5hex=None
-    def hash(self):
-        if self.md5hex is None:
-            self.md5hex = GetMD5Sum(self.name)
-
-
-
-def GetDuplicates(roots):
     # generate the file list
-    result=[]
+    result = []
     for i in FileList(roots):
         result.append(FileObject(i))
 
     # remove files with unique sizes from the list
-    result=filter(lambda x: map(lambda y: y.size, result).count(x.size)>1, result)
+    result = filter(lambda x: 
+                    map(lambda y: 
+                        y.size, result).count(x.size)>1, 
+                  result)
         
     
     # generate the hashes for possible duplicates
     map(lambda x: x.hash(), result)
 
     # generate the hash dictionary
-    hashlist={}
+    hashlist = {}
     for i in range(len(result)):
-        hashlist[result[i].md5hex]=[]
+        hashlist[result[i].md5hex] = []
 
     # populate the hash dictionary with 
     #  key   -> md5 hex digest
@@ -77,8 +82,10 @@ def GetDuplicates(roots):
 
 
     # remove all the keys of length 1 (i.e., files with no duplicates)
+    # compact but slower
+    #hashlist=dict(filter(lambda x: len(x[1])>1, hashlist.items() ))   
     for each in hashlist.keys():
-        if len(hashlist[each])==1:
+        if len(hashlist[each]) == 1:
             del(hashlist[each])
 
     # return a dictionary of with 
@@ -86,7 +93,9 @@ def GetDuplicates(roots):
     #   keys   : the md5 hexdigest
     return hashlist
 
-if __name__=='__main__':
-    roots = ['/home/subimal/Desktop/Backup' ]
-    a=GetDuplicates(roots)
-    print a
+if __name__ == '__main__':
+    roots = ['/home/subimal/Music/', '/home/subimal/Documents/apps/']
+    a = GetDuplicates(roots)
+    for x,y in a.items():
+        print '\n'.join(y)
+        print '<>'*7
